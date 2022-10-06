fd008d55f525244128f474261c76a2976de4e809
Jon Herron <jon.herron@yahoo.com>
2022-10-05 21:21:12 -0400

    Tweaks

--- connectors/connector-invoicepdf/connector_invoicepdf/commands/createAndUploadToS3.py
+++ connectors/connector-invoicepdf/connector_invoicepdf/commands/createAndUploadToS3.py
@@ -3,16 +3,19 @@ from connector_createpdf.commands.create import CreatePDF
 
 class CreateAndUploadToS3:
     def __init__(self, template: str, name: str, amount: str, 
-            aws_bucket: str, aws_access_key_id: str, aws_secret_access_key: str):
+        aws_bucket: str, aws_object_name: str, aws_access_key_id: str, aws_secret_access_key: str):
+        
         self.template = template
         self.name = name
         self.amount = amount
         self.aws_bucket = aws_bucket
+        self.aws_object_name = aws_object_name
         self.aws_access_key_id = aws_access_key_id
         self.aws_secret_access_key = aws_secret_access_key
 
     def execute(self):
-        pdf_result = CreatePDF(self.template).execute()
+        pdf_data = self.template.format(name=self.name, amount=self.amount)
+        pdf_result = CreatePDF(pdf_data).execute()
 
         if pdf_result['status'] != '200':
             return {
@@ -25,6 +28,13 @@ class CreateAndUploadToS3:
             self.aws_secret_access_key, 
             pdf_result['response'],
             self.aws_bucket,
-            'invoice.pdf').execute()
+            self.aws_object_name).execute()
 
-        return aws_result
+        if aws_result['status'] != '200':
+            return aws_result
+
+        return {
+            'response': '{ "created": "' + self.aws_object_name + '"}',
+            'status': '200',
+            'mimetype': 'application/json',
+        }
