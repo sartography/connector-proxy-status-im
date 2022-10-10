5078c3794f9a37c99fc8a5fae65de24413553b66
jbirddog <100367399+jbirddog@users.noreply.github.com>
2022-10-10 10:04:32 -0400

    Consolidate pdf connectors, remove explicit parameters (#5)
    * Drop params
    
    * Renaming
    
    * Consolidate pdf connectors

--- connectors/connector-pdf/connector_pdf/commands/create.py
+++ connectors/connector-pdf/connector_pdf/commands/create.py
@@ -1,12 +1,35 @@
-from connector_aws.commands.uploadFile import UploadFileData
-from connector_createpdf.commands.create import CreatePDF
+from io import BytesIO
+from xhtml2pdf import pisa
 
-class CreateAndUploadToS3:
-    def __init__(self, template: str, name: str, amount: str, aws_object_name: str):
+from connector_aws.commands.uploadFile import UploadFileData
+
+class CreatePDF:
+    def __init__(self, template: str):
+        self.template = template
+
+    def execute(self, config):
+        buf = BytesIO()
+
+        pisa_status = pisa.CreatePDF(self.template, dest=buf)
+
+        if pisa_status.err:
+            return {
+                'response': 'ERR',
+                'status': '500',
+                'mimetype': 'text',
+            }
+        
+        return {
+            'response': buf.getvalue(),
+            'status': '200',
+            'mimetype': 'application/pdf',
+        }
+
+
+class CreatePDFAndUploadToS3:
+    def __init__(self, template: str, aws_object_name: str):
         
         self.template = template
-        self.name = name
-        self.amount = amount
         self.aws_object_name = aws_object_name
 
     def execute(self, config):
@@ -14,8 +37,7 @@ class CreateAndUploadToS3:
         aws_secret_access_key = config['AWS_SECRET_ACCESS_KEY']
         aws_bucket = config['AWS_INVOICE_S3_BUCKET']
 
-        pdf_data = self.template.format(name=self.name, amount=self.amount)
-        pdf_result = CreatePDF(pdf_data).execute(config)
+        pdf_result = CreatePDF(self.template).execute(config)
 
         if pdf_result['status'] != '200':
             return {
@@ -38,3 +60,4 @@ class CreateAndUploadToS3:
             'status': '200',
             'mimetype': 'application/json',
         }
+
