0364f43f7d5474d607f01be3c36be51ffadd077a
Jon Herron <jon.herron@yahoo.com>
2022-12-19 13:13:12 -0500

    Unsafe select values command

--- connectors/connector-postgresql/connector_postgresql/commands/selectValues.py
+++ connectors/connector-postgresql/connector_postgresql/commands/selectValues.py
@@ -1,8 +1,8 @@
 import json
 import psycopg2
 
-class InsertValues:
-    """InsertValues."""
+class SelectValues:
+    """SelectValues."""
 
     def __init__(self, table_name: str, schema: str):
         """__init__."""
@@ -12,18 +12,18 @@ class InsertValues:
     def execute(self, config, task_data):
 
         columns = self._column_definitions(self.schema)
-        (placeholders, values) = self._insert_values(self.schema)
 
         # TODO: build properly with SQL().format(Identifier(name))
         # https://www.psycopg.org/docs/usage.html#passing-parameters-to-sql-queries
-        sql = f"INSERT INTO {self.table_name} ({columns}) VALUES {placeholders};"
+        # TODO: where
+        sql = f"SELECT {columns} FROM {self.table_name};"
 
         with psycopg2.connect(self._get_db_connection_str(config)) as conn:
             with conn.cursor() as cursor:
-                cursor.execute(sql, values)
-                conn.commit
+                cursor.execute(sql)
+                result = json.dumps(self._prep_results(cursor.fetchall()))
 
-        return {"response": f'{{"result": "inserted into {self.table_name}"}}', "status": 200, "mimetype": "application/json"}
+        return {"response": f'{{"result": {result}}}', "status": 200, "mimetype": "application/json"}
 
     def _get_db_connection_str(self, config):
         database = config["POSTGRESQL_DB_NAME"]
@@ -35,11 +35,5 @@ class InsertValues:
     def _column_definitions(self, schema):
         return ",".join(schema["columns"])
 
-    def _insert_values(self, schema):
-        def to_placeholders(values_list):
-            return f"({','.join(['%s'] * len(values_list))})"
-        value_lists = schema["values"]
-        placeholders = ",".join(map(to_placeholders, value_lists))
-        values = [v for value_list in value_lists for v in value_list]
-
-        return (placeholders, values)
+    def _prep_results(self, results):
+        return list(map(list, results))
