1c20e93fb82d488e22f9c03a65619b6d4c893ad1
jasquat <jasquat@users.noreply.github.com>
2023-02-13 14:52:00 -0500

    send a contact request whenever sending a waku message w/ burnettk

--- connectors/connector-waku/connector_waku/commands/sendMessage.py
+++ connectors/connector-waku/connector_waku/commands/sendMessage.py
@@ -31,35 +31,39 @@ class SendMessage:
     message_type: str
     recipient: list[str]
 
+    def send_message(self, message_type_to_use: str, rec: str, message_to_send: str) -> None:
+        url = f'{current_app.config["WAKU_BASE_URL"]}'
+        headers = {"Accept": "application/json", "Content-type": "application/json"}
+        request_body = {
+            "jsonrpc": "2.0",
+            "method": message_type_to_use,
+            "params": [{"id": rec, "message": message_to_send}],
+            "id": 1,
+        }
+
+        try:
+            raw_response = requests.post(url, json.dumps(request_body), headers=headers)
+            status_code = raw_response.status_code
+            parsed_response = json.loads(raw_response.text)
+            response = parsed_response
+        except Exception as ex:
+            response = {"error": str(ex)}
+            status_code = 500
+        return (response, status_code)
+
     def execute(self, config, task_data):
         """Execute."""
         responses = []
         all_calls_returned_200 = True
         for rec in self.recipient:
-            url = f'{current_app.config["WAKU_BASE_URL"]}'
-            headers = {"Accept": "application/json", "Content-type": "application/json"}
-            request_body = {
-                "jsonrpc": "2.0",
-                "method": self.message_type,
-                "params": [{"id": rec, "message": self.message}],
-                "id": 1,
-            }
-
-            try:
-                raw_response = requests.post(url, json.dumps(request_body), headers=headers)
-                status_code = raw_response.status_code
-                if status_code != 200:
-                    all_calls_returned_200 = False
-                parsed_response = json.loads(raw_response.text)
-                response = parsed_response
-            except Exception as ex:
-                response = {"error": str(ex)}
-                status_code = 500
+            response, status_code = self.send_message('wakuext_sendContactRequest', rec, 'Contact Request')
+            if status_code == 200:
+                response, status_code = self.send_message(self.message_type, rec, self.message)
+            if status_code != 200:
                 all_calls_returned_200 = False
 
             responses.append({
                 "response": response,
-                "node_returned_200": True,
                 "status": status_code,
             })
         return ({
