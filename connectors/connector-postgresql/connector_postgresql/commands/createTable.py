ee4dbf44ccad410979eefd9259b020a5448fcb21
Jon Herron <jon.herron@yahoo.com>
2023-02-14 11:53:03 -0500

    Pass secrets into Create/DropTable

--- connectors/connector-postgresql/connector_postgresql/commands/createTable.py
+++ connectors/connector-postgresql/connector_postgresql/commands/createTable.py
@@ -3,13 +3,27 @@ import json
 from typing import Any
 from typing import Dict
 
-from connector_postgresql.baseCommand import BaseCommand
+from connector_postgresql.baseCommand import BaseCommand, ConnectionConfig
 
 class CreateTable(BaseCommand):
     """CreateTable."""
 
-    def __init__(self, table_name: str, schema: Dict[str, Any]):
+    def __init__(self,
+        database_name: str,
+        database_host: str,
+        database_port: int,
+        database_user: str,
+        database_password: str,
+        table_name: str,
+        schema: Dict[str, Any]
+    ):
         """__init__."""
+        self.connection_config = ConnectionConfig(
+            database_name, 
+            database_host, 
+            database_port, 
+            database_user, 
+            database_password)
         self.table_name = table_name
         self.schema = schema
 
@@ -20,7 +34,7 @@ class CreateTable(BaseCommand):
         # https://www.psycopg.org/docs/usage.html#passing-parameters-to-sql-queries
         sql = f"CREATE TABLE {self.table_name} ({columns});"
 
-        return self.execute_query(sql, config)
+        return self.execute_query(sql, self.connection_config)
 
     def _column_definitions(self, schema):
         def column_defintion(column):
--- connectors/connector-postgresql/connector_postgresql/commands/dropTable.py
+++ connectors/connector-postgresql/connector_postgresql/commands/dropTable.py
@@ -1,12 +1,25 @@
 import json
 
-from connector_postgresql.baseCommand import BaseCommand
+from connector_postgresql.baseCommand import BaseCommand, ConnectionConfig
 
 class DropTable(BaseCommand):
     """DropTable."""
 
-    def __init__(self, table_name: str):
+    def __init__(self,
+        database_name: str,
+        database_host: str,
+        database_port: int,
+        database_user: str,
+        database_password: str,
+        table_name: str
+    ):
         """__init__."""
+        self.connection_config = ConnectionConfig(
+            database_name, 
+            database_host, 
+            database_port, 
+            database_user, 
+            database_password)
         self.table_name = table_name
 
     def execute(self, config, task_data):
@@ -15,5 +28,5 @@ class DropTable(BaseCommand):
         # https://www.psycopg.org/docs/usage.html#passing-parameters-to-sql-queries
         sql = f"DROP TABLE {self.table_name};"
 
-        return self.execute_query(sql, config)
+        return self.execute_query(sql, self.connection_config)
 
